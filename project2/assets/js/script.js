9a554bad60b93c1a6c8562dd3ae72d907015b67e
AJMCoder <>
2023-10-25 11:42:18 +0000

    Add basic structure to JavaScript file. Add some styling changes to font and header.

--- project2/assets/js/script.js
+++ project2/assets/js/script.js
@@ -1,1 +1,20 @@
 console.log("Connected!")
+
+const choices = document.querySelectorAll(".choice");
+const roundResult = document.getElementById("round-result");
+
+function playGame(e) {
+
+}
+
+function getComputerChoice() {
+
+}
+
+function getWinner(playerChoice, computerChoice) {
+
+}
+
+function displayResult(playerChoice, computerChoice, winner) {
+    
+}
